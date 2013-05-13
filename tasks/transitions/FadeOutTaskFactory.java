53152c2aa5838d811f0187fd94fd8ef8daf22203
Nitesh Patel <bitbucket@nitesh.name>
2013-05-13 20:32:59 +0100

    FadeOutTaskFactory passes an existing task to new task, make sure that existing task is always reset whenever we pass it

--- tasks/transitions/FadeOutTaskFactory.java
+++ tasks/transitions/FadeOutTaskFactory.java
@@ -1,6 +1,7 @@
 package com.herodevelop.hdfw.tasks.transitions;
 
 import com.herodevelop.hdfw.ScreenEx;
+import com.herodevelop.hdfw.tasks.ReuseTask;
 import com.herodevelop.hdfw.tasks.Task;
 import com.herodevelop.hdfw.tasks.TaskFactory;
 
@@ -8,9 +9,9 @@ public final class FadeOutTaskFactory extends TaskFactory {
 
     private final ScreenEx screen;
     private final int paintIndex;
-    private final Task task;
+    private final ReuseTask task;
 
-    public FadeOutTaskFactory(ScreenEx screen, int paintIndex, Task task) {
+    public FadeOutTaskFactory(ScreenEx screen, int paintIndex, ReuseTask task) {
         this.screen = screen;
         this.paintIndex = paintIndex;
         this.task = task;
@@ -18,6 +19,7 @@ public final class FadeOutTaskFactory extends TaskFactory {
 
     @Override
     public final Task newTask() {
+        task.reset();
         return new FadeOutTask(screen, paintIndex, task);
     }
 }
