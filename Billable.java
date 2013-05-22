4505b9d8973eac6f586d978e7f390b9e54e4f424
Nitesh Patel <bitbucket@nitesh.name>
2013-05-22 23:16:58 +0100

    More reliable way to check if we can start new async tasks

--- Billable.java
+++ Billable.java
@@ -4,15 +4,13 @@ public interface Billable {
 
     public void startSetup();
 
-    public boolean isGdxIabSetupFinished();
-
     public void queryInventoryAsync();
-
-    public boolean isGdxQueryInventoryFinished();
 
     public void consumeAsync();
 
-    public boolean isGdxConsumeFinished();
-
     public void launchPurchaseFlow();
+
+    public boolean isAsyncInProgress();
+
+    public boolean isIabHelperSetup();
 }
