25056f222ce5416926a2e182190116c3b094b52a
Nitesh Patel <bitbucket@nitesh.name>
2013-06-02 18:54:22 +0100

    Add a helper class to create 'color' instances

---
+++ utils/Color.java
@@ -0,0 +1,13 @@
+package com.herodevelop.hdfw.utils;
+
+public class Color {
+    public float red;
+    public float green;
+    public float blue;
+
+    public Color(float red, float green, float blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
